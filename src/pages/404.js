204192919cd0056148d2259f7455307afcb6c4a5
Sima <msima@deloittece.com>
2019-09-28 16:02:15 +0300

    Initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const ErrorPage = () => {
+  return (
+    <Layout>
+      <h1>You've been 404'd</h1>
+    </Layout>
+  )
+}
+
+export default ErrorPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
 import React from "react"
+import Layout from "../components/Layout"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <Layout>
+    Hello world!
+    <Link to="/post">Post Page</Link>
+  </Layout>
+)
